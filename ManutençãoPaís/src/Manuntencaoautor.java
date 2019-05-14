ceac190554a9f9f8b0a90427b9d4c3407e5c0c85
Administrador <Administrador@10.10.4.27>
2019-05-14 14:45:13 -0300

    kjk

--- ManutençãoPaís/src/Manuntencaoautor.java
+++ ManutençãoPaís/src/Manuntencaoautor.java
@@ -8,12 +8,12 @@
  *
  * @author Administrador
  */
-public class ManuntençãoAutor extends javax.swing.JDialog {
+public class Manuntencaoautor extends javax.swing.JDialog {
 
     /**
      * Creates new form ManuntençãoAutor
      */
-    public ManuntençãoAutor(java.awt.Frame parent, boolean modal) {
+    public Manuntencaoautor(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -131,20 +131,23 @@ public class ManuntençãoAutor extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ManuntençãoAutor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Manuntencaoautor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ManuntençãoAutor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Manuntencaoautor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ManuntençãoAutor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Manuntencaoautor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ManuntençãoAutor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Manuntencaoautor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ManuntençãoAutor dialog = new ManuntençãoAutor(new javax.swing.JFrame(), true);
+                Manuntencaoautor dialog = new Manuntencaoautor(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
--- ManutençãoPaís/src/Manutencaopais.java
+++ ManutençãoPaís/src/Manutencaopais.java
@@ -8,12 +8,12 @@
  *
  * @author Administrador
  */
-public class ManuntençãoPaís extends javax.swing.JDialog {
+public class Manutencaopais extends javax.swing.JDialog {
 
     /**
      * Creates new form ManuntençãoPaís
      */
-    public ManuntençãoPaís(java.awt.Frame parent, boolean modal) {
+    public Manutencaopais(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -121,20 +121,23 @@ public class ManuntençãoPaís extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ManuntençãoPaís.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Manutencaopais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ManuntençãoPaís.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Manutencaopais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ManuntençãoPaís.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Manutencaopais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ManuntençãoPaís.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Manutencaopais.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ManuntençãoPaís dialog = new ManuntençãoPaís(new javax.swing.JFrame(), true);
+                Manutencaopais dialog = new Manutencaopais(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
